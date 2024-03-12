53e75888e18f6b6355474ea390dd17cdbac80ada
Agata Wajda <agawaj98@gmail.com>
2024-03-12 23:28:14 +0100

    added fonts, basic colors, modern normalizer and media

---
+++ src/styles/globalStyles.js
@@ -0,0 +1,24 @@
+import { css } from 'styled-components';
+
+export const colors = {
+  backgroundColor: `#f6f7fb`,
+  brandOrange: `#ff751d`,
+  lightOrange: `#FFDAC0`,
+  textBasicGray: `#52555F`,
+  alertColor: `#EB5757`,
+  moneyGreen: `#407946`,
+  moneyRed: `#E7192E`,
+  iconsBlue: `#071F41`,
+};
+
+export const above768px = styles => css`
+  @media screen and (min-width: 768px) {
+    ${styles}
+  }
+`;
+
+export const above1280px = styles => css`
+  @media screen and (min-width: 1280px) {
+    ${styles}
+  }
+`;
