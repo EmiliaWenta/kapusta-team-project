08607b84d001bce73080cd3a1b4bbe2e6f055d09
KacperTrybus <117526502+KacperTrybus@users.noreply.github.com>
2024-03-17 00:05:19 +0100

    App to init state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,16 @@
-import { ReportBox } from './ReportBox/ReportBox';
-
 export const App = () => {
   return (
     <div
-    // style={{
-    //   // height: '100vh',
-    //   display: 'flex',
-    //   justifyContent: 'center',
-    //   alignItems: 'center',
-    //   fontSize: 40,
-    //   color: '#010101',
-    // }}
+      style={{
+        // height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
     >
-      {/* React homework template */}
-      <ReportBox />
+      React homework template
     </div>
   );
 };
