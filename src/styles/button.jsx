40693b50b980afb02d44873aa3d673b3272cb683
Emilia Wenta <emiliakielinska96@gmail.com>
2024-03-18 11:58:07 +0100

    Create balance component

--- src/styles/button.jsx
+++ src/styles/button.jsx
@@ -4,7 +4,7 @@ import { colors, above768px, above1280px } from './globalStyles';
 export const StyledButton = styled.button`
   color: ${colors.textBasicGray};
   font-size: 12px;
-  font-weight: 700px;
+  font-weight: 700;
   text-transform: uppercase;
   height: 44px;
   width: 125px;
@@ -33,6 +33,36 @@ export const StyledGreyButton = styled(StyledButton)`
   ${above768px(css`
     width: 122px;
   `)}
+`;
+
+export const StyledGreyButtonWithR = styled(StyledGreyButton)`
+  color: rgba(82, 85, 95, 0.7);
+  border-radius: 0px 22px 22px 0px;
+  width: 142px;
+  padding: 12px 16px;
+  text-align: left;
+  border-top: 2px solid white;
+  border-right: 2px solid white;
+  border-bottom: 2px solid white;
+  border-left: none;
+  outline: none;
+  box-shadow: none;
+  &:focus {
+  color: #ffffff;
+  background-color:${colors.brandOrange};
+  }
+  &:hover {
+    color: #ffffff;
+    background-color:${colors.brandOrange};
+  }
+  ${above768px(css`
+    width: 125px;
+    border-radius: 22px;
+    text-align: center;
+    padding: 0px;
+  `)}
+  
+}
 `;
 
 export const StyledTomatoButtonWithShadow = styled(StyledTomatoButton)`
