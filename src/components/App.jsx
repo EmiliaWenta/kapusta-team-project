5e8e37cffbd83663eb3fe4301fafefa604f11a1a
KacperTrybus <117526502+KacperTrybus@users.noreply.github.com>
2024-03-19 21:15:05 +0100

    App to init

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,21 @@
-// import { Route, Routes } from 'react-router-dom';
-import ExpSummaryGrid from './ExpSummaryGrid/ExpSummaryGrid';
+import { Route, Routes } from 'react-router-dom';
+// import ExpSummaryGrid from './ExpSummaryGrid/ExpSummaryGrid';
 
-// import Home from 'pages/Home';
-// import Layout from 'components/Layout/Layout';
-// import { RestricterRoute } from './RestrictedRoute';
+import Home from 'pages/Home';
+import Layout from 'components/Layout/Layout';
+import { RestricterRoute } from './RestrictedRoute';
 
 export const App = () => {
   return (
     <div>
-      {/* <Routes>
+      <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<RestricterRoute component={Home} path="" />} />
 
           <Route path="*" element={<Home />} />
         </Route>
-      </Routes> */}
-      <ExpSummaryGrid />
+      </Routes>
+      {/* <ExpSummaryGrid /> */}
     </div>
   );
 };
