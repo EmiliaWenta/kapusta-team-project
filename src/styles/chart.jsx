92a055b8b608eb697e3e2647609c4c54ae1a7153
Emilia Wenta <emiliakielinska96@gmail.com>
2024-03-21 21:56:25 +0100

    fix overflow problem

--- src/styles/chart.jsx
+++ src/styles/chart.jsx
@@ -2,11 +2,25 @@ import styled, { css } from 'styled-components';
 import { above768px } from './globalStyles';
 
 export const StyledChartContainer = styled.div`
+  overflow-y: auto;
+  height: 900px;
+  box-sizing: border-box;
   width: 280px;
 
   ${above768px(css`
-    max-width: 605px;
     overflow-x: auto;
-    height: 350px;
+    width: 1000px;
+    height: 400px;
+  `)}
+`;
+
+export const StyledDiv = styled.div`
+  height: 490px;
+  overflow: auto;
+  width: 300px;
+  ${above768px(css`
+    width: 690px;
+    overflow: auto;
+    height: 420px;
   `)}
 `;
