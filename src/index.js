c03f60d1dcf4b7a5f932b773932477207916ee89
RadekKisiela <radek1997_97@o2.pl>
2024-03-21 19:31:44 +0100

    Add GoogleOAuthProvider to index.js

--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { persistor, store } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <PersistGate persistor={persistor}>
       <BrowserRouter basename="/kapusta-team-project">
-        <App />
+        <GoogleOAuthProvider clientId="959704838575-frda8dks6298cc1rjkji9qiahskmca7r.apps.googleusercontent.com">
+          <App />
+        </GoogleOAuthProvider>
       </BrowserRouter>
     </PersistGate>
   </Provider>
