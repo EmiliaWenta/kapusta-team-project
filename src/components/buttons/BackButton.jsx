7fed50ccfb15c0eba7dc2d7007bba72243a2a739
KacperTrybus <117526502+KacperTrybus@users.noreply.github.com>
2024-03-23 17:22:23 +0100

    Upd Navlink for btn

--- src/components/buttons/BackButton.jsx
+++ src/components/buttons/BackButton.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const BackButton = ({ leftArrowIcon, buttonText, addStyles, addStylesDiv }) => {
   return (
-    <div style={addStylesDiv}>
-      <svg width="18" height="18">
-        <use href={leftArrowIcon} />
-      </svg>
-      <span style={addStyles}>{buttonText}</span>
-    </div>
+    <NavLink to="/finance" style={{ 'text-decoration': 'none' }}>
+      <div style={addStylesDiv}>
+        <svg width="18" height="18">
+          <use href={leftArrowIcon} />
+        </svg>
+        <span style={addStyles}>{buttonText}</span>
+      </div>
+    </NavLink>
   );
 };
 
