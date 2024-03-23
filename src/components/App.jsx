4b3eade2d4c6cf86b5ece7725772112202dde816
KacperTrybus <117526502+KacperTrybus@users.noreply.github.com>
2024-03-23 13:46:41 +0100

    app to init

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,26 @@
-// import { Route, Routes } from 'react-router-dom';
-// import { useSelector } from 'react-redux';
-//
-// import Home from 'pages/Home';
-// import Layout from 'components/Layout/Layout';
-// import Finance from 'pages/Finance';
-// import { selectIsLogged } from '../redux/selectors';
-import ReportPage from 'pages/ReportPage';
+import { Route, Routes } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+import Home from 'pages/Home';
+import Layout from 'components/Layout/Layout';
+import Finance from 'pages/Finance';
+import { selectIsLogged } from '../redux/selectors';
 
 export const App = () => {
-  // const isLoggedIn = useSelector(selectIsLogged);
+  const isLoggedIn = useSelector(selectIsLogged);
 
   return (
     <div>
-      {/* <Routes> */}
-      {/* <Route path="/" element={<Layout />}> */}
-      {/* {isLoggedIn ? ( */}
-      {/* // <Route path="/" element={<Finance />} /> */}
-      {/* // ) : ( */}
-      {/* // <Route path="/" element={<Home />} /> */}
-      {/* // )} */}
-      {/* <Route path="*" element={<Home />} /> */}
-      {/* </Route> */}
-      {/* </Routes> */}
-      <ReportPage />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {isLoggedIn ? (
+            <Route path="/" element={<Finance />} />
+          ) : (
+            <Route path="/" element={<Home />} />
+          )}
+          <Route path="*" element={<Home />} />
+        </Route>
+      </Routes>
     </div>
   );
 };
